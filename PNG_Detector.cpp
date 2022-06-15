2f55e6f6250a081863221e890b17207859808404
AW1ng <87144424+AW1ng@users.noreply.github.com>
2022-06-15 18:59:50 +0300

    Update PNG_Detector.cpp

--- PNG_Detector.cpp
+++ PNG_Detector.cpp
@@ -28,27 +28,10 @@ void PNG_detector(string path) {
 
 
 int main() {
-	char* path = "C:\\Users\\Alex_dev\\Pictures\\God of War\\1.png";
-//	char* path = "C:\\Users\\Alex_dev\\Desktop\\GitCommands.txt";
+	char* path = ".png";
+//	char* path = ".txt";
 	PNG_detector(path);
 }
 
 
-//	std::size_t pos = path.find(".");
-//	string str = path.substr(pos);
-//	picture.seekg(pos, picture.end);
-//	int length = picture.tellg();
-//	picture.seekg(pos, picture.beg);
-	
-	
-//	void HexOut(const char* c, int len) {
-//	for(int i = 0; i < len; i++) {
-//		cout.width(2);
-//		cout.fill('0');
-//		cout << hex << static_cast<unsigned short>(c[i]) <<' ';
-//		if(c[0] == -119) cout << "PNG";
-//	}
-//	if(is_png(c)) cout << "PNG";
-//	else cout << "Not PNG";
-//}
 
